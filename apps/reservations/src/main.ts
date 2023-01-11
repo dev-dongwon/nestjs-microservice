12d1b5dd1b8fe7619d7dc6799843655488eb7ea5
dev-dongwon <dev.dongwonkim@gmail.com>
2023-01-11 16:12:23 +0900

    Feat: Add validation

--- apps/reservations/src/main.ts
+++ apps/reservations/src/main.ts
@@ -5,7 +5,8 @@ import { Logger } from 'nestjs-pino';
 
 async function bootstrap() {
   const app = await NestFactory.create(ReservationsModule);
-  app.useGlobalPipes(new ValidationPipe());
+  // whitelist: 클래스에 선언된 변수들만 처리하고 그 외의 변수들은 버림
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useLogger(app.get(Logger));
   await app.listen(3000);
 }
