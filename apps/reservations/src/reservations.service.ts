760ab22c3785cd155badc4f88fd52618042e83b0
dev-dongwon <dev.dongwonkim@gmail.com>
2023-01-14 13:23:24 +0900

    Feat: Add auth module

--- apps/reservations/src/reservations.service.ts
+++ apps/reservations/src/reservations.service.ts
@@ -7,30 +7,30 @@ import { ReservationsRepository } from './reservations/reservations.repository';
 export class ReservationsService {
   constructor(private readonly reservationRepository: ReservationsRepository) {}
 
-  create(createReservationDto: CreateReservationDto) {
-    return this.reservationRepository.create({
+  async create(createReservationDto: CreateReservationDto) {
+    return await this.reservationRepository.create({
       ...createReservationDto,
       timestamp: new Date(),
       userId: '123',
     });
   }
 
-  findAll() {
-    return this.reservationRepository.find({});
+  async findAll() {
+    return await this.reservationRepository.find({});
   }
 
-  findOne(_id: string) {
-    return this.reservationRepository.findOne({ _id });
+  async findOne(_id: string) {
+    return await this.reservationRepository.findOne({ _id });
   }
 
-  update(_id: string, updateReservationDto: UpdateReservationDto) {
-    return this.reservationRepository.findOneAndUpdate(
+  async update(_id: string, updateReservationDto: UpdateReservationDto) {
+    return await this.reservationRepository.findOneAndUpdate(
       { _id },
       { $set: updateReservationDto },
     );
   }
 
-  remove(_id: string) {
-    return this.reservationRepository.findOneAndDelete({ _id });
+  async remove(_id: string) {
+    return await this.reservationRepository.findOneAndDelete({ _id });
   }
 }
