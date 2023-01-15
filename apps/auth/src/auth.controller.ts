c6555ce85c17668a10d78bcf604b2b41ad57ccef
dev-dongwon <dev.dongwonkim@gmail.com>
2023-01-15 20:12:03 +0900

    Feat: ADd user local login

--- apps/auth/src/auth.controller.ts
+++ apps/auth/src/auth.controller.ts
@@ -1,10 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Controller()
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @UseGuards()
+  @Post('login')
+  
   @Get()
   getHello(): string {
     return this.authService.getHello();
--- apps/auth/src/users/users.service.ts
+++ apps/auth/src/users/users.service.ts
@@ -1,12 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 import { createUserDto } from './dto/create-user.dto';
 import { UsersRepository } from './users.repository';
+import { UserDocument } from './models/user.schema';
 
 @Injectable()
 export class UsersService {
   constructor(private readonly userRepository: UsersRepository) {}
 
   async create(createUserDto: createUserDto) {
-    return await this.userRepository.create(createUserDto);
+    return await this.userRepository.create({
+      ...createUserDto,
+      password: await bcrypt.hash(createUserDto.password, 10),
+    });
+  }
+
+  async verifyUser(email: string, password: string): Promise<UserDocument> {
+    const user = await this.userRepository.findOne({ email });
+    const passwordIsValid = await bcrypt.compare(password, user.password);
+
+    if (!passwordIsValid) {
+      throw new UnauthorizedException('Credentials are not valid');
+    }
+
+    return user;
   }
 }
