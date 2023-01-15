60ceedf4ce4b300076f38161136c9acd8cd18c2e
dev-dongwon <dev.dongwonkim@gmail.com>
2023-01-15 18:02:14 +0900

    Modified: modify config module

--- apps/reservations/src/main.ts
+++ apps/reservations/src/main.ts
@@ -2,12 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { ReservationsModule } from './reservations.module';
 import { ValidationPipe } from '@nestjs/common';
 import { Logger } from 'nestjs-pino';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(ReservationsModule);
   // whitelist: 클래스에 선언된 변수들만 처리하고 그 외의 변수들은 버림
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useLogger(app.get(Logger));
-  await app.listen(3000);
+
+  const configService = app.get(ConfigService);
+  await app.listen(configService.get('PORT'));
 }
 bootstrap();
