d5328d4e91db5216fddb18ea47b59d34592d46f4
dev-dongwon <dev.dongwonkim@gmail.com>
2023-01-20 15:23:11 +0900

    move: strategy folder move under src

--- apps/auth/src/strategies/jwt.strategy.ts
+++ apps/auth/src/strategies/jwt.strategy.ts
@@ -2,25 +2,31 @@ import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
-import { UsersService } from '../src/users/users.service';
-import { Request } from 'express';
-import { TokenPayload } from '../src/interfaces/token-payload.interface';
+import { TokenPayload } from '../interfaces/token-payload.interface';
+import { UsersService } from '../users/users.service';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(
     configService: ConfigService,
-    private readonly userService: UsersService,
+    private readonly usersService: UsersService,
   ) {
     super({
       jwtFromRequest: ExtractJwt.fromExtractors([
-        (request: Request) => request?.cookies?.Authentication,
+        (request: any) => {
+          console.log('=================request', request);
+          return (
+            request?.cookies?.Authentication ||
+            request?.Authentication ||
+            request?.headers.Authentication
+          );
+        },
       ]),
       secretOrKey: configService.get('JWT_SECRET'),
     });
   }
 
   async validate({ userId }: TokenPayload) {
-    return await this.userService.getUser({ _id: userId });
+    return this.usersService.getUser({ _id: userId });
   }
 }
