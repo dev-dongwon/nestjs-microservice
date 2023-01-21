3471c93931add9b87f6fa46fc0f5a49adcde7c1a
dev-dongwon <dev.dongwonkim@gmail.com>
2023-01-21 19:41:17 +0900

    Fix: remove console.log

--- apps/auth/src/users/users.controller.ts
+++ apps/auth/src/users/users.controller.ts
@@ -1,36 +1,20 @@
-import {
-  Body,
-  Controller,
-  Get,
-  Post,
-  Req,
-  Res,
-  UseGuards,
-} from '@nestjs/common';
-import { createUserDto } from './dto/create-user.dto';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { CurrentUser } from '../current-user.decorator';
+import { CurrentUser } from '../../../../libs/common/src/decorators/current-user.decorator';
 import { UserDocument } from './models/user.schema';
 import { JwtAuthGuard } from '../guards/jwt-auth.guard';
-import { Request, Response } from 'express';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  async createUser(@Body() createUserDto: createUserDto) {
+  async createUser(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
 
-  @Get('/cookies')
-  getCookies(@Req() req: Request, @Res() res: Response): any {
-    const jwt = req.cookies['Authentication'];
-    return res.send(jwt);
-  }
-
   @UseGuards(JwtAuthGuard)
-  @Get()
   async getUser(@CurrentUser() user: UserDocument) {
     return user;
   }
