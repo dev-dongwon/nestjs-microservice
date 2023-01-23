289d9133020a2cd61b2ea05805adfa64ffa98f8a
dev-dongwon <dev.dongwonkim@gmail.com>
2023-01-23 11:26:24 +0900

    Feat: Add payment charge on controller

--- apps/payments/src/payments.controller.ts
+++ apps/payments/src/payments.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UsePipes, ValidationPipe } from '@nestjs/common';
 import { PaymentsService } from './payments.service';
+import { MessagePattern, Payload } from '@nestjs/microservices';
 
 @Controller()
 export class PaymentsController {
   constructor(private readonly paymentsService: PaymentsService) {}
 
-  
+  @MessagePattern('create_charge')
+  @UsePipes(new ValidationPipe())
+  async createCharge(@Payload() data: PaymentsCreateChargeDto) {
+    return this.paymentsService.createCharge(data);
+  }
 }
